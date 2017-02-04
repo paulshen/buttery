7cfeb623a0743f6929c6003e24894652b4b17b8c
Paul Shen <paulshen0@gmail.com>
2017-02-04 15:19:20 -0800

    chrome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 /* @flow */
 import React from 'react';
+import Radium from 'radium';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
-      <div>
-        Hi!
+      <div style={Styles.Root}>
+        <div style={Styles.Chrome}>
+        </div>
       </div>
     );
   }
+}
+export default Radium(App);
+
+const Styles = {
+  Root: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    justifyContent: 'center',
+  },
+  Chrome: {
+    border: '1px solid #cccccc',
+    height: '667px',
+    width: '375px',
+  },
 }
