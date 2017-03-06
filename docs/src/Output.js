16ef29119b7b000ccabfcc43aad06002bff3f43b
Paul Shen <paulshen0@gmail.com>
2017-03-05 21:20:05 -0800

    add proto to docs

--- docs/src/Output.js
+++ docs/src/Output.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Radium from 'radium';
 
+import { Layer } from './proto';
+
 function Output() {
   return (
     <div style={Styles.Root}>
+      <Layer
+        properties={{
+          x: 0,
+          y: 0,
+          width: 80,
+          height: 80,
+          backgroundColor: 'blue',
+        }}
+      />
     </div>
   );
 }
