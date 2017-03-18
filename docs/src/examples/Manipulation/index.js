fc522b0bcf4120f9c4cf8cd2e6c5c8cfa7511ff5
Paul Shen <paulshen0@gmail.com>
2017-03-18 09:54:57 -0700

    [docs] Manipulation

--- docs/src/examples/Manipulation/index.js
+++ docs/src/examples/Manipulation/index.js
@@ -1,8 +1,25 @@
+import React from 'react';
+
+import { Section, Paragraph, InlineCode, Header } from '../../components/Description';
+import CodeComponent from '../../Code';
 import Code from './Code';
 import App from './App';
 
 export default {
   name: 'Manipulation',
+  description: () => (
+    <div>
+      <Section>
+        <Paragraph>Layers can be updated just like any other React component. No surprises here!</Paragraph>
+        <Paragraph>For performance, Wasabi does a property-level equality check to determine whether to update.</Paragraph>
+      </Section>
+      <CodeComponent>
+{`<Layer
+  properties={MyProperties[this.state.index]}
+/>`}
+      </CodeComponent>
+    </div>
+  ),
   App,
   Code,
 };
