372f95d2138ad69ed39a3cf8711852965b614bab
Paul Shen <paulshen0@gmail.com>
2017-03-18 09:32:54 -0700

    [docs] codemirror

--- docs/src/Code.js
+++ docs/src/Code.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Radium from 'radium';
 
-function Code({ children }) {
-  return (
-    <pre>
-      <code style={Styles.Code}>
-        {children}
-      </code>
-    </pre>
-  );
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/jsx/jsx';
+import 'codemirror/lib/codemirror.css';
+
+class Code extends React.Component {
+  componentDidMount() {
+    this._codemirror = CodeMirror.fromTextArea(this._textarea, {
+      mode: 'javascript',
+      viewportMargin: Infinity,
+    });
+  }
+
+  componentWillUnmount() {
+    this._codemirror.toTextArea();
+  }
+
+  render() {
+    let { children } = this.props;
+
+    return (
+      <textarea
+        style={Styles.Code}
+        ref={c => this._textarea = c}
+        readOnly={true}
+        value={children}
+      />
+    );
+  }
 }
 export default Radium(Code);
 
