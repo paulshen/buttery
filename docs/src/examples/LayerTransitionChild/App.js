b82374ae266dcfdc1668440cf6b2d84ce64ce8ec
Paul Shen <paulshen0@gmail.com>
2017-03-20 09:17:51 -0700

    [docs] update LayerTransitionChild example

--- docs/src/examples/LayerTransitionChild/App.js
+++ docs/src/examples/LayerTransitionChild/App.js
@@ -3,47 +3,55 @@ import ReactTransitionGroup from 'react-addons-transition-group';
 
 import { Layer, LayerTransitionChild, SpringAnimator } from '../../proto';
 
-const OverlayProperties = {
-  x: 0,
-  y: 100,
-  width: 375,
-  height: 567,
-  backgroundColor: 'lightblue',
-};
+function getProperties(i) {
+  return {
+    x: 47.5 + 60 * (i % 5),
+    y: 40 + 60 * Math.floor(i / 5),
+    width: 40,
+    height: 40,
+    backgroundColor: 'lightblue',
+    opacity: 1,
+  };
+}
 
 export default class Example extends React.Component {
   state = {
-    showOverlay: false,
+    numStart: 0,
+    numEnd: 0,
   };
 
-  _onClick = () => {
+  _onClickAdd = () => {
     this.setState({
-      showOverlay: !this.state.showOverlay,
+      numEnd: this.state.numEnd + 1,
+    });
+  };
+
+  _onClickRemove = () => {
+    this.setState({
+      numStart: Math.min(this.state.numStart + 1, this.state.numEnd),
     });
   };
 
   render() {
+    let layers = [];
+    for (let i = 0; i < this.state.numEnd - this.state.numStart; i++) {
+      layers.push(
+        <LayerTransitionChild
+          enterProperties={{ ...getProperties(i), opacity: 0 }}
+          properties={getProperties(i)}
+          exitProperties={{ ...getProperties(i), opacity: 0 }}
+          animator={SpringAnimator({ spring: 0.0001, friction: 0.02 })}
+          key={this.state.numStart + i}
+        />
+      );
+    }
+
     return (
-      <Layer
-        properties={{ x: 0, y: 0, width: 375, height: 667 }}
-        style={{ overflow: 'hidden' }}>
-        <button onClick={this._onClick}>Toggle</button>
+      <Layer properties={{ x: 0, y: 0, width: 375, height: 667 }}>
+        <button onClick={this._onClickAdd}>Add</button>
+        <button onClick={this._onClickRemove}>Remove</button>
         <ReactTransitionGroup>
-          {this.state.showOverlay &&
-            <LayerTransitionChild
-              enterProperties={{
-                ...OverlayProperties,
-                y: 667,
-              }}
-              properties={OverlayProperties}
-              exitProperties={{
-                ...OverlayProperties,
-                x: 375,
-              }}
-              animator={SpringAnimator({ spring: 0.0001, friction: 0.02 })}
-              key="overlay"
-            />
-          }
+          {layers}
         </ReactTransitionGroup>
       </Layer>
     );
