17e0c09d15348d4c607494d901d63f20f2514777
Paul Shen <paulshen0@gmail.com>
2017-05-26 18:31:16 -0400

    remove proto from docs

--- docs/src/proto/index.js
+++
@@ -1,7 +0,0 @@
-/* @flow */
-
-export { default as Layer } from './Layer';
-export { default as LayerTransitionChild } from './LayerTransitionChild';
-export { default as SpringAnimator } from './animators/SpringAnimator';
-export { default as TimedAnimator } from './animators/TimedAnimator';
-export { default as DragConstraint } from './DragConstraint';
