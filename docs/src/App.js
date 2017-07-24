21814d9bfc8aed9317e54f881be15f8a91ad684a
Paul Shen <paulshen0@gmail.com>
2017-07-24 10:16:28 -0700

    [docs] basename configuration

--- docs/src/App.js
+++ docs/src/App.js
@@ -7,7 +7,9 @@ import DocumentationPage from './DocumentationPage';
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Router
+        basename={process.env.NODE_ENV === 'production' ? '/buttery' : null}
+      >
         <div>
           <Route path="/docs" component={DocumentationPage} />
           <Route path="/example" component={HomePage} />
